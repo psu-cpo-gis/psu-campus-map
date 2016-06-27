46ca2da41e2d376891d5287f14f8a0276c3078c3
Kevin Saavedra <kms22@psu.ds.pdx.edu>
2016-06-27 14:49:48 -0700

    Changed gruntfile js to local CPO machine (desktop)

--- gruntfile.js
+++ gruntfile.js
@@ -91,7 +91,7 @@ module.exports = function(grunt) {
     },
     compass: {
       options: {
-        basePath: "c:/Users/kevin/Campus_GIS/psu-campus-map/",
+        basePath: "c:/Users/kms22/Campus_GIS/psu-campus-map/",
         sassDir: "<%= yeoman.app %>/styles",
         cssDir: ".tmp/styles",
         generatedImagesDir: ".tmp/styles/ui/images/",
